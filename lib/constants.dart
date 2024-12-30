b66e66dc47e9d3138ba47919bf4ed44d20fca4d8
PREMIER CMPUTER <hanadarya905@gmail.com>
2024-12-30 03:59:35 -0800

    Initial commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 //TODO: 1.3.3 Put your API key and CoinAPI's base url in the following constants
-const kAPIKey = '';
-const kBaseURL = '';
+const kAPIKey = '0FFCE0EB-777B-46F7-85FF-02BA5C53351B';
+const kBaseURL = 'https://rest.coinapi.io/v1/exchangerate';
--- lib/utilities/coin_data.dart
+++ lib/utilities/coin_data.dart
@@ -1,3 +1,8 @@
+import 'package:coin_tracker/constants.dart';
+import 'package:coin_tracker/models/coin_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -29,16 +34,36 @@ const List<String> cryptoList = [
 ];
 
 class CoinData {
-  //TODO: 1.0 Create a function with no return type, called getCoinData, which takes a String parameter called currency
-  //This function is used to retrieve the exchange rates for each every coin, including bitcoin, ether, and litecoin
-  //TODO: 1.2 Create an empty List of type CoinModel called cryptoPrices
-  //TODO: 1.3 Use a For In loop that loops through the cryptoList
-  //Step 1.3.1 is inside pubspec.yaml
-  //Step 1.3.2 is inside constants.dart
-  //TODO: 1.3.3 Inside the loop make a request to the server to get the exchange for the currency in the current loop
-  //TODO: 1.3.4 Check if the status code of the response is 200, otherwise print the status code
-  //TODO: 1.3.5 Inside the IF block, decode the response to a JSON object
-  //TODO: 1.3.6 Create a CoinModel object and give it an icon using loop's current crypto name. give it the coin name, and the coin price
-  //You can retrieve the icon name using "split" method. for example splitting BTC from "BTC/Bitcoin". You may google to see how split works.
-  //TODO: 1.4 At the end of the function, after the loop, return the cryptoPrices
+  Future getCoinData(String currency) async {
+    List<CoinModel> cryptoPrices = [];
+    for (String crypto in cryptoList) {
+      String cryptoSign = crypto.split('/')[0];
+      String requestExchange =
+          '$kBaseURL/$cryptoSign/$currency?apikey=$kAPIKey';
+      http.Response response = await http.get((Uri.parse(requestExchange)));
+      if (response.statusCode == 200) {
+        String cryptoName = crypto.split('/')[1];
+        var data = jsonDecode(response.body);
+        double price = data['rate'];
+        CoinModel coins =
+            CoinModel(icon: cryptoSign, name: cryptoName, price: price);
+        cryptoPrices.add(coins);
+      } else {
+        print(response.statusCode);
+      }
+    }
+    return cryptoPrices;
+  }
 }
+//TODO: 1.0 Create a function with no return type, called getCoinData, which takes a String parameter called currency
+//This function is used to retrieve the exchange rates for each every coin, including bitcoin, ether, and litecoin
+//TODO: 1.2 Create an empty List of type CoinModel called cryptoPrices
+//TODO: 1.3 Use a For In loop that loops through the cryptoList
+//Step 1.3.1 is inside pubspec.yaml
+//Step 1.3.2 is inside constants.dart
+//TODO: 1.3.3 Inside the loop make a request to the server to get the exchange for the currency in the current loop
+//TODO: 1.3.4 Check if the status code of the response is 200, otherwise print the status code
+//TODO: 1.3.5 Inside the IF block, decode the response to a JSON object
+//TODO: 1.3.6 Create a CoinModel object and give it an icon using loop's current crypto name. give it the coin name, and the coin price
+//You can retrieve the icon name using "split" method. for example splitting BTC from "BTC/Bitcoin". You may google to see how split works.
+//TODO: 1.4 At the end of the function, after the loop, return the cryptoPrices
